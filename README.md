2939229004b5d0bbd6aa6ef747c6576fdba0d052
Phil Sinatra <phil@epvisual.com>
2014-02-28 12:17:41 -0500

    Continued UI development
    - navigation styles and functionality
    - background image classes and images established
    - social media icons integrated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Red, White and Brew
 ===========
 
+Social media icons from:
+[Dawid Dapszus - Dribbbl](http://dribbble.com/shots/1209419-20-Social-Media-Icons-Freebie)
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
   $('#btn-oc').sidr({
-    displace : false
+    displace  : false,
+    speed     : 'fast'
   });
 
   // Click event handlers ------------------------------------------------------
+  //
+  /**
+   * Programatically close the sidr menu when close button is clicked
+   * @param  {event} e default click event
+   * @return {null}   no return
+   */
   $('.sidr-close').click(function(e) {
     e.preventDefault();
     $.sidr('close', 'sidr');
   });
+});
+
+$('nav #nav-main a').click(function(e) {
+  e.preventDefault();
+  var targetSection = $(this).attr('href').substring(1);
+  $('body').attr('class', '');
+  $('body').addClass(targetSection);
 });
